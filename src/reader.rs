9830c345f507ea66e5ddec683de72fd936ffe0b8
Ashwin Naren <arihant2math@gmail.com>
2025-12-31 13:58:05 -0800

    write trait

--- src/reader.rs
+++ src/reader.rs
@@ -36,7 +36,22 @@ pub trait Ext4Read: Send + Sync {
     ) -> Result<(), BoxedError>;
 }
 
-/// Error type used by the [`Vec<u8>`] impl of [`Ext4Read`].
+// TODO: Move this someplace else
+/// Interface used by [`Ext4`] to write the filesystem data to a storage
+/// file or device.
+///
+/// [`Ext4`]: crate::Ext4
+#[async_trait]
+pub trait Ext4Write: Send + Sync {
+    /// Write bytes from `src`, starting at `start_byte`.
+    async fn write(
+        &mut self,
+        start_byte: u64,
+        src: &[u8],
+    ) -> Result<(), BoxedError>;
+}
+
+/// Error type used by the [`Vec<u8>`] impls of [`Ext4Read`] and [`Ext4Write`].
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct MemIoError {
     start: u64,
@@ -74,10 +89,41 @@ impl Ext4Read for Vec<u8> {
     }
 }
 
+#[async_trait]
+impl Ext4Write for Vec<u8> {
+    async fn write(
+        &mut self,
+        start_byte: u64,
+        src: &[u8],
+    ) -> Result<(), BoxedError> {
+        write_to_bytes(self, start_byte, src).ok_or_else(|| {
+            Box::new(MemIoError {
+                start: start_byte,
+                read_len: src.len(),
+                src_len: self.len(),
+            })
+            .into()
+        })
+    }
+}
+
 fn read_from_bytes(src: &[u8], start_byte: u64, dst: &mut [u8]) -> Option<()> {
     let start = usize::try_from(start_byte).ok()?;
     let end = start.checked_add(dst.len())?;
     let src = src.get(start..end)?;
+    dst.copy_from_slice(src);
+
+    Some(())
+}
+
+fn write_to_bytes(
+    dst: &mut [u8],
+    start_byte: u64,
+    src: &[u8],
+) -> Option<()> {
+    let start = usize::try_from(start_byte).ok()?;
+    let end = start.checked_add(src.len())?;
+    let dst = dst.get_mut(start..end)?;
     dst.copy_from_slice(src);
 
     Some(())
