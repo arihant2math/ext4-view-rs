afba59281a9c31b989ad613ad89788fde955a886
Ashwin Naren <arihant2math@gmail.com>
2026-02-12 11:47:09 -0800

    write block groups

--- src/block_group.rs
+++ src/block_group.rs
@@ -6,18 +6,20 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use crate::Ext4Read;
 use crate::block_index::FsBlockIndex;
 use crate::checksum::Checksum;
 use crate::error::{CorruptKind, Ext4Error};
 use crate::features::{IncompatibleFeatures, ReadOnlyCompatibleFeatures};
 use crate::superblock::Superblock;
 use crate::util::{
-    read_u16le, read_u32le, u32_from_hilo, u64_from_hilo, usize_from_u32,
+    read_u16le, read_u32le, u32_from_hilo, u32_to_hilo, u64_from_hilo,
+    usize_from_u32, write_u16le,
 };
+use crate::{Ext4, Ext4Read};
 use alloc::vec;
 use alloc::vec::Vec;
 use core::ops::Deref;
+use core::ops::DerefMut;
 
 pub(crate) type BlockGroupIndex = u32;
 
@@ -38,8 +40,18 @@ impl Deref for BlockGroupDescriptorBytes {
     }
 }
 
+impl DerefMut for BlockGroupDescriptorBytes {
+    fn deref_mut(&mut self) -> &mut [u8] {
+        match self {
+            BlockGroupDescriptorBytes::OnDisk32(bytes) => bytes,
+            BlockGroupDescriptorBytes::OnDisk64(bytes) => bytes,
+        }
+    }
+}
+
 #[derive(Debug)]
 pub(crate) struct BlockGroupDescriptor {
+    index: BlockGroupIndex,
     bytes: BlockGroupDescriptorBytes,
 }
 
@@ -53,13 +65,18 @@ impl BlockGroupDescriptor {
     ///
     /// # Panics
     /// If `bytes` is not at least [`Self::SIZE_IN_BYTES_ON_DISK`] bytes long.
-    fn from_bytes(superblock: &Superblock, bytes: &[u8]) -> Self {
+    fn from_bytes(
+        superblock: &Superblock,
+        index: BlockGroupIndex,
+        bytes: &[u8],
+    ) -> Self {
         if superblock
             .incompatible_features()
             .contains(IncompatibleFeatures::IS_64BIT)
         {
             assert_eq!(bytes.len(), Self::SIZE_IN_BYTES_ON_DISK_64);
             Self {
+                index,
                 bytes: BlockGroupDescriptorBytes::OnDisk64(
                     bytes[..Self::SIZE_IN_BYTES_ON_DISK_64].try_into().unwrap(),
                 ),
@@ -67,10 +84,38 @@ impl BlockGroupDescriptor {
         } else {
             assert_eq!(bytes.len(), Self::SIZE_IN_BYTES_ON_DISK_32);
             Self {
+                index,
                 bytes: BlockGroupDescriptorBytes::OnDisk32(
                     bytes[..Self::SIZE_IN_BYTES_ON_DISK_32].try_into().unwrap(),
                 ),
             }
+        }
+    }
+
+    fn update_checksum(&mut self, superblock: &Superblock) {
+        if superblock
+            .read_only_compatible_features()
+            .contains(ReadOnlyCompatibleFeatures::METADATA_CHECKSUMS)
+        {
+            let mut checksum = Checksum::with_seed(superblock.checksum_seed());
+            checksum.update_u32_le(self.index);
+            // Up to the checksum field.
+            checksum.update(&self.bytes[..Self::BG_CHECKSUM_OFFSET]);
+            // Zero'd checksum field.
+            checksum.update_u16_le(0);
+            // Rest of the block group descriptor.
+            checksum.update(&self.bytes[Self::BG_CHECKSUM_OFFSET + 2..]);
+            // Truncate to the lower 16 bits.
+            let checksum = u16::try_from(checksum.finalize() & 0xffff).unwrap();
+            self.bytes[Self::BG_CHECKSUM_OFFSET..Self::BG_CHECKSUM_OFFSET + 2]
+                .copy_from_slice(&checksum.to_le_bytes());
+        } else if superblock
+            .read_only_compatible_features()
+            .contains(ReadOnlyCompatibleFeatures::GROUP_DESCRIPTOR_CHECKSUMS)
+        {
+            unimplemented!(
+                "Support for the GROUP_DESCRIPTOR_CHECKSUMS feature is not yet implemented"
+            );
         }
     }
 
@@ -142,6 +187,27 @@ impl BlockGroupDescriptor {
         u32_from_hilo(bg_free_blocks_count_hi, bg_free_blocks_count_lo)
     }
 
+    pub(crate) fn set_free_blocks_count(
+        &mut self,
+        superblock: &Superblock,
+        count: u32,
+    ) {
+        const LO_OFFSET: usize = 0xC;
+        const HI_OFFSET: usize = 0x2C;
+        let (hi, lo) = u32_to_hilo(count);
+        match &mut self.bytes {
+            BlockGroupDescriptorBytes::OnDisk32(bytes) => {
+                assert_eq!(hi, 0);
+                write_u16le(bytes, LO_OFFSET, lo);
+            }
+            BlockGroupDescriptorBytes::OnDisk64(bytes) => {
+                write_u16le(bytes, HI_OFFSET, hi);
+                write_u16le(bytes, LO_OFFSET, lo);
+            }
+        };
+        self.update_checksum(superblock);
+    }
+
     pub(crate) fn free_inodes_count(&self) -> u32 {
         const LO_OFFSET: usize = 0xE;
         const HI_OFFSET: usize = 0x2E;
@@ -159,6 +225,27 @@ impl BlockGroupDescriptor {
         u32_from_hilo(bg_free_inodes_count_hi, bg_free_inodes_count_lo)
     }
 
+    pub(crate) fn set_free_inodes_count(
+        &mut self,
+        superblock: &Superblock,
+        count: u32,
+    ) {
+        const LO_OFFSET: usize = 0xE;
+        const HI_OFFSET: usize = 0x2E;
+        let (hi, lo) = u32_to_hilo(count);
+        match &mut self.bytes {
+            BlockGroupDescriptorBytes::OnDisk32(bytes) => {
+                assert_eq!(hi, 0);
+                write_u16le(bytes, LO_OFFSET, lo);
+            }
+            BlockGroupDescriptorBytes::OnDisk64(bytes) => {
+                write_u16le(bytes, HI_OFFSET, hi);
+                write_u16le(bytes, LO_OFFSET, lo);
+            }
+        };
+        self.update_checksum(superblock);
+    }
+
     pub(crate) fn used_dirs_count(&self) -> u32 {
         const LO_OFFSET: usize = 0x10;
         const HI_OFFSET: usize = 0x30;
@@ -174,7 +261,28 @@ impl BlockGroupDescriptor {
         u32_from_hilo(bg_used_dirs_count_hi, bg_used_dirs_count_lo)
     }
 
-    pub(crate) fn checksum(&self) -> u16 {
+    pub(crate) fn set_used_dirs_count(
+        &mut self,
+        superblock: &Superblock,
+        count: u32,
+    ) {
+        const LO_OFFSET: usize = 0x10;
+        const HI_OFFSET: usize = 0x30;
+        let (hi, lo) = u32_to_hilo(count);
+        match &mut self.bytes {
+            BlockGroupDescriptorBytes::OnDisk32(bytes) => {
+                assert_eq!(hi, 0);
+                write_u16le(bytes, LO_OFFSET, lo);
+            }
+            BlockGroupDescriptorBytes::OnDisk64(bytes) => {
+                write_u16le(bytes, HI_OFFSET, hi);
+                write_u16le(bytes, LO_OFFSET, lo);
+            }
+        };
+        self.update_checksum(superblock);
+    }
+
+    fn checksum(&self) -> u16 {
         read_u16le(self.bytes.deref(), Self::BG_CHECKSUM_OFFSET)
     }
 
@@ -214,7 +322,7 @@ impl BlockGroupDescriptor {
             .ok_or(CorruptKind::BlockGroupDescriptor(bgd_index))?;
         reader.read(start, &mut data).await.map_err(Ext4Error::Io)?;
 
-        let block_group_descriptor = Self::from_bytes(sb, &data);
+        let block_group_descriptor = Self::from_bytes(sb, bgd_index, &data);
 
         let has_metadata_checksums = sb
             .read_only_compatible_features()
@@ -249,6 +357,19 @@ impl BlockGroupDescriptor {
         }
 
         Ok(block_group_descriptor)
+    }
+
+    pub(crate) async fn write(&mut self, ext4: &Ext4) -> Result<(), Ext4Error> {
+        let start = Self::get_start_byte(&ext4.0.superblock, self.index)
+            .ok_or(CorruptKind::BlockGroupDescriptor(self.index))?;
+        self.update_checksum(&ext4.0.superblock);
+        // Write only the data we've saved to avoid overwriting any unread info
+        let writer = ext4.0.writer.as_ref().ok_or(Ext4Error::Readonly)?;
+        writer
+            .write(start, &self.bytes)
+            .await
+            .map_err(Ext4Error::Io)?;
+        Ok(())
     }
 
     /// Read all block group descriptors.
